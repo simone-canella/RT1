779898a18666a2957d6a77a8b473d03ae5636eca
Simone Canella <simone.canella@gmail.com>
2025-12-03 18:26:27 +0100

    Initialized the turtle's position, changed logic to Boolean logic NOTE: Still a problem with trajectory

--- ros2_workspace/src/turtlesim_controller/src/exercise1.cpp
+++ ros2_workspace/src/turtlesim_controller/src/exercise1.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp" 
 #include "turtlesim/msg/pose.hpp" // what we want to read (subscribe)
 #include "geometry_msgs/msg/twist.hpp" // what we want to write (publish)
+#include "turtlesim/srv/teleport_absolute.hpp" // for setting the position
 #include <iostream>
 #include <functional>
 
@@ -19,11 +20,27 @@ public:
         // 2. PUBLISHER: send velocity commands
         publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(
             "/turtle1/cmd_vel", 10);
-        
-        // 3. TIMER: control loop (runs every 100 ms)
+        // 3. CLIENT: setting the position
+        teleport_client_ = this->create_client<turtlesim::srv::TeleportAbsolute>("/turtle1/teleport_absolute");
+
+        while (!teleport_client_->wait_for_service(std::chrono::seconds(1))) {
+            RCLCPP_WARN(this->get_logger(), "Waiting for /turtle1/teleport_absolute service...");
+        }
+
+        auto request = std::make_shared<turtlesim::srv::TeleportAbsolute::Request>();
+        request->x = 2.5; //not "1.0" because the turning condition
+        request->y = 1;
+        request->theta = 0.0;
+
+        auto result = teleport_client_->async_send_request(request);
+
+        // 4. TIMER: control loop (runs every 100 ms)
         timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100), 
             std::bind(&Controller::timer_callback, this)); 
+        
+        turning_ = false;
+        moving_right_ = true;
     }
 
 private:
@@ -32,11 +49,19 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 
-    float x_;
-    float y_;
-    float theta_;
+    rclcpp::Client<turtlesim::srv::TeleportAbsolute>::SharedPtr teleport_client_; // client for setting position
+
+    //STATE VARIABLES
+    float x_ = 0;
+    float y_ = 0;
+    float theta_ = 0;
+
+    bool turning_; // true = executing a semicircle
+    bool moving_right_ ; // false = moving left
+
+    //CONTROL PARAMETERS
     float v_ = 2.0;
-    float omega_ = 1;
+    float omega_ = 1.0;
 
     // === CALLBACKS ===
 
@@ -58,16 +83,42 @@ private:
     {
         geometry_msgs::msg::Twist message;
 
-        // CLOSED-LOOP LOGIC
-        if (x_ > 9.0) { //turning motion
-            message.linear.x = v_;
-            message.angular.z = omega_;
-        } else if (x_ < 2.0) { //turning motion
-            message.linear.x = v_;
-            message.angular.z = -omega_;
-        } else { //forward motion
+
+        if (y_ > 10.5) { //stop when reach top limit
+            publisher_->publish(geometry_msgs::msg::Twist());
+            RCLCPP_INFO(this->get_logger(), "Stop: reached top limit");
+            return;
+        }
+        
+        if (turning_) { //turning logic
+            if (moving_right_) {
+                if (theta_ < 3.1) { //anti-clock turning
+                    message.linear.x = v_;
+                    message.angular.z = omega_;
+                } else {
+                    turning_ = false; // finished turn
+                    moving_right_ = false; // now going left
+                }
+            } else {
+                if (theta_ > 0.1) { //clock turning
+                    message.linear.x = v_;
+                    message.angular.z = -omega_;
+                } else {
+                    turning_ = false; // finished turn
+                    moving_right_ = true; // now going left
+                }
+            }
+        } else { //straight motion
             message.linear.x = v_;
             message.angular.z = 0.0;
+
+            if (moving_right_ && x_ > 9.0) { // right boundary reached
+                turning_ = true; //start turning logic
+            }
+
+            if (!moving_right_ && x_ < 2.0) { // left boundary reached
+                turning_ = true; //start turning logic
+            }
         }
 
         publisher_->publish(message);
