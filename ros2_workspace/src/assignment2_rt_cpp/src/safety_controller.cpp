8f1c9ebddbd4ca23a06f2df58aa43833bf549a50
Simone Canella <simone.canella@gmail.com>
2026-01-21 17:17:36 +0100

    Create safety_controller node and verify startup

--- ros2_workspace/src/assignment2_rt_cpp/src/safety_controller.cpp
+++ ros2_workspace/src/assignment2_rt_cpp/src/safety_controller.cpp
@@ -1,10 +1,20 @@
 #include <rclcpp/rclcpp.hpp>
 #include "geometry_msgs/msg/twist.hpp"
+#include <iostream>
+
+class SafetyController : public rclcpp::Node {
+public:
+    SafetyController() : Node("safety_controller")
+    {
+        RCLCPP_INFO(this->get_logger(), "Safety Controller started");
+    }
+
+};
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = rclcpp::Node::make_shared("teleop_interface");
+    auto node = std::make_shared<SafetyController>();
 
     rclcpp::spin(node);
 
