de74dcc49bfaaeb096609cc683e3430748bfd127
Simone Canella <simone.canella@gmail.com>
2026-04-29 10:45:40 +0200

    Implement UI input loop and goal-sending logic

--- ros2_workspace/src/assignment1_rt2/src/action_client.cpp
+++ ros2_workspace/src/assignment1_rt2/src/action_client.cpp
@@ -31,6 +31,9 @@ namespace assignment1_rt2
                 RCLCPP_ERROR(this->get_logger(), "Action server not available!");
                 return;
             }
+
+            // launch a new thread for UI loop (avoiding block execution)
+            std::thread{std::bind(&RobotActionClient::ui_loop, this)}.detach();
         }
 
     private:
@@ -38,8 +41,44 @@ namespace assignment1_rt2
         // create the action client object
         rclcpp_action::Client<RobotTarget>::SharedPtr client_ptr_;
 
-        void ui_loop() {
-            
+        // INTERNAL FUNCTIONS
+        // loop for user's inputs
+        void ui_loop()
+        {
+
+            while (rclcpp::ok())
+            {
+                double x, y, theta;
+
+                std::cout << "\n ---- TARGET INPUT ----" << std::endl;
+                std::cout << "Enter X coordinate: ";
+                std::cin >> x;
+                std::cout << "Enter Y coordinate: ";
+                std::cin >> y;
+                std::cout << "Enter the orientation coordinate: ";
+                std::cin >> theta;
+
+                RCLCPP_INFO(this->get_logger(), "User input received: x= %f, y=%f, theta=%f", x, y, theta);
+
+                // call the function to send the goal
+                this->send_goal(x, y, theta);
+            }
+        }
+
+        // send the goal position to the action server
+        void send_goal(double x, double y, double theta)
+        {
+            // create goal message
+            auto goal_msg = RobotTarget::Goal();
+
+            goal_msg.x = x;
+            goal_msg.y = y;
+            goal_msg.theta = theta;
+
+            RCLCPP_INFO(this->get_logger(), "Sending goal coordinates to the server...");
+
+            // send message
+            this->client_ptr_->async_send_goal(goal_msg);
         }
     };
 }
